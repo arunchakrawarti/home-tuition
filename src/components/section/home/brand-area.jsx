b18f9b0a5e96933fd06872ec91367bed3ef78d22
arunchakrawarti <arunmojampur@gmail.com>
2026-06-11 14:29:22 +0530

    feat:chnage the image acoording to given doc

--- src/components/section/home/brand-area.jsx
+++ src/components/section/home/brand-area.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React, { useRef } from "react";
+import React from "react";
 import Slider from "react-slick";
 
 const BrandArea = () => {
@@ -12,39 +12,51 @@ const BrandArea = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
+    arrows: false,
+
     responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2.35,
-          slidesToScroll: 1,
+          slidesToShow: 2.2,
         },
       },
     ],
-    arrows: false,
   };
 
   return (
-    <section className="w-full flex flex-col text-center gap-3 sm:gap-5 pb-5 pt-2 ">
+    <section className="w-full flex flex-col text-center gap-3 sm:gap-5 pb-5 pt-2">
       <h2 className="heading">Trusted by Clients</h2>
 
-      {/* slide  */}
-
-      <div className="w-full py-1">
+      {/* Slider */}
+      <div className="w-full py-2">
         <Slider {...settings} className="outline-none">
           {clients?.map((data, index) => (
             <div
               key={index}
-              className="pr-1 sm:pr-3 outline-none rounded-full overflow-hidden"
+              className="px-2 outline-none overflow-hidden"
             >
-              <div className="rounded-full px-3 sm:px-5 py-4 bg-primaryLightest overflow-hidden flex-center">
+              {/* Bigger Card */}
+              <div className="bg-primaryLightest h-[150px] sm:h-[120px] rounded-3xl flex items-center justify-center px-6">
                 <Image
                   title={data?.name}
                   alt={data?.name}
-                  width={100}
-                  height={50}
+                  width={320}
+                  height={180}
                   src={`/assets/clients/${data?.logo}`}
-                  className="h-[50px] w-auto rounded-lg"
+                  className="h-[120px] sm:h-[190px] w-auto object-contain"
                 />
               </div>
             </div>
@@ -60,26 +72,6 @@ export default BrandArea;
 const clients = [
   {
     name: "",
-    logo: "vertex.webp",
-  },
-  {
-    name: "",
-    logo: "airindia.svg",
-  },
-  {
-    name: "",
-    logo: "nexa.png",
-  },
-  {
-    name: "",
-    logo: "styky.png",
-  },
-  {
-    name: "",
-    logo: "np.webp",
-  },
-  {
-    name: "",
     logo: "1.png",
   },
   {
@@ -90,16 +82,24 @@ const clients = [
     name: "",
     logo: "3.png",
   },
-  // {
-  //   name: "",
-  //   logo: "4.png",
-  // },
-  // {
-  //   name: "",
-  //   logo: "5.png",
-  // },
-  // {
-  //   name: "instantdoctor24/7",
-  //   logo: "instantdoctor.png",
-  // },
+  {
+    name: "",
+    logo: "4.png",
+  },
+  {
+    name: "",
+    logo: "5.png",
+  },
+  {
+    name: "",
+    logo: "6.png",
+  },
+  {
+    name: "",
+    logo: "7.png",
+  },
+  {
+    name: "",
+    logo: "8.png",
+  },
 ];
